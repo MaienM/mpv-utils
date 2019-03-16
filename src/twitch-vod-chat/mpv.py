9688e1f880f7d891e12a77247cdbba21cadd9252
Michon van Dooren <michon1992@gmail.com>
2019-03-16 22:44:48 +0100

    Added events & property observing to MPV wrapper

--- src/twitch-vod-chat/mpv.py
+++ src/twitch-vod-chat/mpv.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 import socket
 import sys
@@ -43,14 +44,27 @@ class MPV(threading.Thread):
 		self.reconnect = reconnect
 		self.stop_requested = threading.Event()
 
-		# Data to be sent to the client. Lock must be used when using this variable.
+		# Data to be sent to the client.
 		self.send_buffer = b''
 		self.send_lock = threading.Lock()
 
-		# Used to store listeners and responses. Lock must be used when using either of these variables.
+		# Used to store listeners and responses.
 		self.request_id = 1
 		self.listeners = {}
 		self.listener_lock = threading.Lock()
+
+		# Used to store event listeners.
+		self.handlers = defaultdict(lambda: [])
+		self.handler_lock = threading.Lock()
+
+		# Used to store observe listeners.
+		self.observer_id = 1
+		self.observers = defaultdict(lambda: [])
+		self.observer_ids = {}
+		self.observer_lock = threading.Lock()
+
+		# Handle property-change events, for the observers.
+		self.handlers['property-change'].append(self._observe_handler)
 
 	def stop(self):
 		self.stop_requested.set()
@@ -80,24 +94,31 @@ class MPV(threading.Thread):
 				while '\n' in buffer:
 					message, buffer = buffer.split('\n', 1)
 					message = json.loads(message)
-					if 'request_id' not in message:
-						self.log.warn(f'Received a message without a request id, ignoring it: {message}')
-						continue
-					request_id = message['request_id']
-					with self.listener_lock:
-						listener = self.listeners[request_id]
-						if listener:
-							self.log.debug(f'Received response for request {request_id}: {message}')
-							listener.set(message)
-						else:
-							self.log.warn(f'Received response for request {request_id}, but there is no listener: {message}')
+					if 'request_id' in message:
+						request_id = message['request_id']
+						with self.listener_lock:
+							listener = self.listeners.get(request_id)
+							if listener:
+								self.log.debug(f'Received response for request {request_id}: {message}')
+								listener.set(message)
+							else:
+								self.log.warn(f'Received response for request {request_id}, but there is no listener: {message}')
+					elif 'event' in message:
+						event = message['event']
+						self.log.debug(f'Received event {event}: {message}')
+						with self.handler_lock:
+							for handler in self.handlers[event]:
+								handler(message)
+					else:
+						self.log.warn(f'Received unknown message: {message}')
 
-	def send(self, data):
+	def _send(self, data):
 		self.log.debug(f'Adding message to send buffer: {data}')
 		with self.send_lock:
 			self.send_buffer += (data + '\n').encode('utf-8')
 
 	def command(self, command, *args):
+		""" Send a command to MPV, and wait for a response. """
 		event = EventWithMessage()
 		with self.listener_lock:
 			request_id = self.request_id
@@ -109,13 +130,58 @@ class MPV(threading.Thread):
 				'command': [command, *args],
 				'request_id': request_id,
 			}
-			self.send(json.dumps(command_data))
+			self._send(json.dumps(command_data))
 
 			response = event.wait(5)
 			if response['error'] != 'success':
-				print(response)
-				raise MPVError(response['data'])
-			return response['data']
+				raise MPVError(response['error'])
+			return response.get('data')
 		finally:
 			with self.listener_lock:
 				del self.listeners[request_id]
+
+	def on(self, event, handler):
+		""" Listen to an MPV event. """
+		self.log.info(f'Adding handler {handler} to event {event}')
+		with self.handler_lock:
+			self.handlers[event].append(handler)
+		return lambda: self.off(event, handler)
+	
+	def off(self, event, handler):
+		""" Stop listening to an MPV event. """
+		self.log.info(f'Removing handler {handler} from event {event}')
+		with self.handler_lock:
+			self.handlers[event].remove(handler)
+
+	def observe(self, prop, handler, request_initial = False):
+		""" Listen to changes to an MPV property. """
+		self.log.info(f'Adding observer {handler} to property {prop}')
+		with self.observer_lock:
+			self.observers[prop].append(handler)
+			if prop not in self.observer_ids:
+				# First observer of this property, so start listening
+				observer_id = self.observer_id
+				self.observer_id += 1
+				self.observer_ids[prop] = observer_id
+				self.command('observe_property', observer_id, prop)
+		if request_initial:
+			handler(self.command('get_property', prop))
+		return lambda: self.unobserve(prop, handler)
+
+	def unobserve(self, prop, handler):
+		""" Stop listening to changes to an MPV property. """
+		self.log.info(f'Removing observer {handler} from property {prop}')
+		with self.observer_lock:
+			self.observers[prop].remove(handler)
+			if not self.observers[prop]:
+				# Last observer of this property, so stop listening
+				observer_id = self.observer_ids[prop]
+				del self.observer_ids[prop]
+				self.command('unobserve-property', observer_id)
+
+	def _observe_handler(self, message):
+		""" Handles all property-change events, and triggers observers from it. """
+		with self.observer_lock:
+			data = message['data']
+			for handler in self.observers[message['name']]:
+				handler(data)
