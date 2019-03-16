4264372ddfc7aad156267adc4e88a0c99df15d23
Michon van Dooren <michon1992@gmail.com>
2019-03-16 16:39:43 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import setuptools
+
+
+setuptools.setup(
+	name = 'mpv-utils',
+	version = '0.1.0',
+	license = 'MIT',
+	description = "Utility functions for MPV",
+	author = 'Michon van Dooren',
+	author_email = 'michon1992@gmail.com',
+	url = 'https://gitlab.com/maienm/mpv-utils',
+	classifiers = [
+		'Development Status :: 3 - Alpha'
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: Unix',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Utilities',
+	],
+	keywords = [
+		'mpv',
+	],
+
+	packages = setuptools.find_packages('src'),
+	package_dir = { '': 'src' },
+	include_package_data = True,
+	zip_safe = False,
+	install_requires = [
+		'configupdater',
+		'nerdfonts',
+		'requests',
+	],
+)
